8ec06c9f0014cbf221f46fc8b47e3713708625e3
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-27 13:57:39 +0500

    page and

--- src/Component/questions/card/index.jsx
+++ src/Component/questions/card/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 function Card() {
+  const [toggle, setToggle] = useState(false);
   const text = () => {
-    const runText = document.getElementsByClassName("question__text");
-    console.log(runText);
+    // const runText = document.getElementsByClassName("question__text");
+    setToggle(!toggle);
   };
   return (
     <div onClick={text} className="question__card-wrapper">
@@ -13,14 +14,18 @@ function Card() {
           <i class="fa-solid fa-chevron-up"></i>
         </span>
       </p>
-      <p className="question__text">
-        Biz sizning ma’lumotlaringizni saqlanishi haqida siz uchun o’ylab
-        qo’yganmiz. Ma’lumotlar bazasini xafvsiz ta’minlash uchun sizning o’quv
-        markazingiz va kompaniyamiz o’rtasida sirlarni saqlash uchun maxfiylik
-        siyosati nomli shartnoma imzolanadi. Bu orqali sizning
-        ma’lumotlaringizni oshkora qilmaslik majburiyatini bizning kompaniyamiz
-        oladi.
-      </p>
+      {toggle ? (
+        <p className="question__text">
+          Biz sizning ma’lumotlaringizni saqlanishi haqida siz uchun o’ylab
+          qo’yganmiz. Ma’lumotlar bazasini xafvsiz ta’minlash uchun sizning
+          o’quv markazingiz va kompaniyamiz o’rtasida sirlarni saqlash uchun
+          maxfiylik siyosati nomli shartnoma imzolanadi. Bu orqali sizning
+          ma’lumotlaringizni oshkora qilmaslik majburiyatini bizning
+          kompaniyamiz oladi.
+        </p>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
--- src/Component/questions/index.jsx
+++ src/Component/questions/index.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./style.css";
 import Card from "./card";
 function Question() {
+  const cards = [<Card />, <Card />, <Card />, <Card />];
   return (
     <div className="question__wrapper">
       <p className="question__title">Tez-tez so’raladigan savollar</p>
-      <Card />
+      <div className="cards__wrapper">{cards}</div>
     </div>
   );
 }
